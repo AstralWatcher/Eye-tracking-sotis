f788f83a2b153ec3dac94b826ca88cea3ef0e97f
Astral <andrijacvejic@gmail.com>
2019-12-04 14:42:33 +0100

    Izmene

--- Data.py
+++ Data.py
@@ -16,6 +16,18 @@ class Region:
   def height(self):
     return self.y2 - self.y1
 
+  def getX1(self):
+    return self.x1
+
+  def getX2(self):
+    return self.x2
+
+  def getY1(self):
+    return self.y1
+
+  def getY2(self):
+    return self.y2
+
 
 class Data:
   def __init__(self, x, y, time, region=Region(0, 0, 0, 0)):
@@ -27,7 +39,51 @@ class Data:
   def print(self):
     print("X:" + str(self.x) + " Y:" + str(self.y) + " Time:" + str(self.time) + " Region: " + str(self.region.print()))
 
+  def setRegion(self,region):
+    self.region = region
+
+  def getX(self):
+    return self.x
+
+  def getY(self):
+    return self.Y
+
+  def getTime(self):
+    return self.time
+
+  def getRegion(self):
+    return self.region
+
+class DataArray:
+  def __init__(self):
+    self.array = []
+
+  #end = true adds to end
+  def insert(self, data,  end=True):
+    at = 0
+    if end:
+      at = self.array.__len__()
+    self.array.insert(at, data)
+
+  def print(self):
+    for i in range(0, self.array.__len__()):
+      self.array[i].print()
+
+  def get(self, elementAt):
+    if elementAt >= self.array.__len__():
+      return -1
+    return self.array[elementAt]
+
 
 R1 = Region(0, 0, 150, 200)
 p1 = Data(15, 21, 0.5, R1)
-p1.print()
+p2 = Data(22, 33, 0.7, R1)
+
+D = DataArray()
+D.insert(p1)
+D.insert(p2)
+D.print()
+
+print(D.get(1).x)
+
+#p1.print()
--- Load.py
+++ Load.py
@@ -1,21 +1,38 @@
 import pandas
 from os import path
+from dateutil import parser
+from Data import Region
+from Data import Data
+from Data import DataArray
+
+arr = DataArray()
+
+R1 = Region(0, 0, 150, 200)
+R2 = Region(151, 201, 201, 251)
+regions = [R1, R2]
+
+
+def readData(filePath):
+  df = pandas.read_csv(filePath)
+  print(df.dtypes.__len__())
+  axes = df.axes
+  print(axes[1])  # axes[1][1] prvi red
+  sstarttime = axes[1][0]
+  print("Start " + str(sstarttime))
+
+  ddate = parser.parse(sstarttime[5:len(sstarttime) - 1])
+  print(ddate)
+
+  for index, row in df.head().iterrows():  # bez head prolazi kroz sve
+    print(index, row[0], row[1], row[2], row[3], row[4], row[5])
+    newData = Data(row[0], row[2], row[3], None)
+    arr.insert(newData)
+
 
 students = ["ANDRIJA", "ACA", "NINA"]
 basePath = path.dirname(__file__)
 filePath = path.abspath(path.join(basePath, "..", "result", "ANDRIJA", "p1", "User 2_all_gaze.csv"))
 
-#print(df.dtypes)
-#axes = df.axes
-#print(axes[1]) #axes[1][1] prvi red
+readData(filePath, regions)
 
-def toRegion(filePath):
-  df = pandas.read_csv(filePath)
-  #print(df.dtypes)
-  axes = df.axes
-  print(axes[1]) #axes[1][1] prvi red
-
-  
-
-
-#print(str(df.loc[0][0]))
+# print(str(df.loc[0][0]))
