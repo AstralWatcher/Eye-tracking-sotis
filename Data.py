7f2ee078b0e14642839c433cfbf4c32b9a1fa229
Astral <andrijacvejic@gmail.com>
2020-02-04 09:43:10 +0100

    Seckanje po vremenu podatke

--- Data.py
+++ Data.py
@@ -1,89 +1,93 @@
 class Region:
-  def __init__(self, x1, y1, x2, y2):
-    if x1 > x2 or y2 > y1:
-      print("Pogresno kreiran region")
-    self.x1 = x1
-    self.x2 = x2
-    self.y1 = y1
-    self.y2 = y2
+    def __init__(self, x1, y1, x2, y2):
+        if x1 > x2 or y2 > y1:
+            print("Pogresno kreiran region")
+            print("X1:" + str(x1) + "X2:" + str(x2) + "Y1:" + str(y1) + "y2:" + str(y2))
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
 
-  def print(self):
-    return "X1:" + str(self.x1) + " Y1:" + str(self.y1) + " X2:" + str(self.x2) + " Y2:" + str(self.y2)
+    def print(self):
+        return "X1:" + str(self.x1) + " Y1:" + str(self.y1) + " X2:" + str(self.x2) + " Y2:" + str(self.y2)
 
-  def width(self):
-    return self.x2 - self.x1
+    def width(self):
+        return self.x2 - self.x1
 
-  def height(self):
-    return self.y2 - self.y1
+    def height(self):
+        return self.y2 - self.y1
 
-  def getX1(self):
-    return self.x1
+    def getX1(self):
+        return self.x1
 
-  def getX2(self):
-    return self.x2
+    def getX2(self):
+        return self.x2
 
-  def getY1(self):
-    return self.y1
+    def getY1(self):
+        return self.y1
 
-  def getY2(self):
-    return self.y2
+    def getY2(self):
+        return self.y2
 
 
 class Data:
-  def __init__(self, x, y, time, region=Region(0, 0, 0, 0)):
-    self.x = x
-    self.y = y
-    self.time = time
-    self.region = region
+    def __init__(self, x, y, time, region=Region(0, 0, 0, 0)):
+        self.x = x
+        self.y = y
+        self.time = time
+        self.region = region
 
-  def print(self):
-    print("X:" + str(self.x) + " Y:" + str(self.y) + " Time:" + str(self.time) + " Region: " + str(self.region.print()))
+    def print(self):
+        print("X:" + str(self.x) + " Y:" + str(self.y) + " Time:" + str(self.time) + " Region: " + str(
+            self.region.print()))
 
-  def setRegion(self,region):
-    self.region = region
+    def setRegion(self, region):
+        self.region = region
 
-  def getX(self):
-    return self.x
+    def getX(self):
+        return self.x
 
-  def getY(self):
-    return self.Y
+    def getY(self):
+        return self.Y
 
-  def getTime(self):
-    return self.time
+    def getTime(self):
+        return self.time
 
-  def getRegion(self):
-    return self.region
+    def getRegion(self):
+        return self.region
+
 
 class DataArray:
-  def __init__(self):
-    self.array = []
+    def __init__(self):
+        self.array = []
 
-  #end = true adds to end
-  def insert(self, data,  end=True):
-    at = 0
-    if end:
-      at = self.array.__len__()
-    self.array.insert(at, data)
+    # end = true adds to end
+    def insert(self, data, end=True):
+        at = 0
+        if end:
+            at = self.array.__len__()
+        self.array.insert(at, data)
 
-  def print(self):
-    for i in range(0, self.array.__len__()):
-      self.array[i].print()
+    def print(self):
+        for i in range(0, self.array.__len__()):
+            self.array[i].print()
 
-  def get(self, elementAt):
-    if elementAt >= self.array.__len__():
-      return -1
-    return self.array[elementAt]
+    def get(self, elementAt):
+        if elementAt >= self.array.__len__():
+            return -1
+        return self.array[elementAt]
 
 
-R1 = Region(0, 0, 150, 200)
-p1 = Data(15, 21, 0.5, R1)
-p2 = Data(22, 33, 0.7, R1)
+if __name__ == "__main__":
+    R1 = Region(0, 0, 150, 200)
+    p1 = Data(15, 21, 0.5, R1)
+    p2 = Data(22, 33, 0.7, R1)
 
-D = DataArray()
-D.insert(p1)
-D.insert(p2)
-D.print()
+    D = DataArray()
+    D.insert(p1)
+    D.insert(p2)
+    D.print()
 
-print(D.get(1).x)
+    # print(D.get(1).x)
 
-#p1.print()
+    # p1.print()
--- splitData.py
+++ splitData.py
@@ -1,8 +1,61 @@
+def make_parser(cls):
+    def parse_commas(text):
+        return cls(re.sub(r'[^-+\d.]', '', text))
+
+    return parse_commas
+
+
 if __name__ == "__main__":
+    import re
     import pandas as pd
+    import os
+    from os import listdir
+    from os.path import isfile, join, isdir
+    import numpy as np
+    import datetime
+    from dateutil import parser
+    from Data import Region
+    from Data import Data
+    from Data import DataArray
 
-    print("Starting to cut")
-    path = "C:\Users\Astral\Desktop\Master\Sotis\sotisDataToCut"
+    path = r"data/questions"
 
-    df = pd.read_excel(io=path) # sheet_name=sheet)
-    print(df.head(5))  # print first 5 rows of the dataframe
+    to_int = make_parser(int)
+
+    time_stapms = []
+    for i in range(1, 35):
+        time_stapms.append("Time " + str(i))
+    df = pd.read_csv(r"data/question_time.csv", parse_dates=[time_stapms])
+
+    # print(df.dtypes.__len__())
+    print(df)
+    axes = df.axes
+    # print(axes[1])  # axes[1][1] prvi red
+    print(df.dtypes)
+
+    basePath = os.path.dirname(os.path.abspath(__file__))
+    filePath = basePath + "\\data\\questions"
+    users = [f for f in listdir(filePath) if isdir(join(filePath, f))]  # if isfile(join(filePath, f))]
+    print(users)
+
+    arr = list()
+    #df.head().itertuples() - More optimized
+    for index, row in df.head().iterrows():  # bez head prolazi kroz sve
+
+        folder_name = "user" + str(row["User"])
+        file_name = "User " + str(row["User"]) + "_all_gaze.csv"
+        print("I=" + str(index), folder_name)  # row[1], row[2], row[3], row[4], row[5])
+        times = row.values[0]
+        list_times = times.split(" ")
+        print(times)
+        dfu = pd.read_csv(filePath + "\\" + folder_name + "\\" + file_name)
+        #dfu.astype({dfu.axes[1][1]:""})
+        print(dfu.axes[1])
+        print("----------")
+        print(dfu.dtypes)
+        print("----------")
+        print(dfu)
+        #TODO parse time
+        #TODO 2, 2 iters to check which time is needed
+        #TODO 3, split in folders
+        # Foreach in timestamp ( load User folder all_gazes and split)
